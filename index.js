ffef5d5f40e6fd9112bd131978549705d549cbd0
Alex Aaron <alexfrancisaaron@gmail.com>
2024-12-20 13:50:58 -0600

    removed solution code

--- index.js
+++ index.js
@@ -3,52 +3,14 @@ Only declare your variables with const and let.
 */
 
 // Problem #1 //
-let getLastMedicationInfo = (patient) => {
-    const last = patient.medications[patient.medications.length - 1];
-    return `${last.name} - ${last.type} - Administer ${last.administration.frequency}`;
-};
+let getLastMedicationInfo;
 
 // Problem #2 //
-let getObjectKeyValues = (patients) => {
-    const output = [];
-    for (let i = patients.length - 1; i >= 0; i-=2){
-        for (let key in patients[i]){
-            if (typeof patients[i][key] === 'boolean'){
-                output.push(`${key}: ${patients[i][key]}`);
-            }
-        }
-    }
-    return output;
-};
+let getObjectKeyValues;
 
 // Problem #3 //
-let getDailyMedications = (patients) => {
-    const output = [];
-    for (let i = 0; i < patients.length; i++){
-        const meds = patients[i].medications;
-        for (let j = 0; j < meds.length; j++){
-            console.log(meds);
-            if (meds[j].administration.frequency === 'Daily'){
-                output.push(meds[j]);
-            }
-        }
-    }
-    return output;
-};
+let getDailyMedications;
 
 // Problem #4 //
-let getOneMedBySpecies = (patients, species) => {
-    const output = [];
-    for (let i = 0; i < patients.length; i++){
-        if (patients[i].species === species){
-            if (patients[i].medications.length === 1){
-                output.push({
-                    patient: `${patients[i].name} - ${patients[i].species}`,
-                    medication: `${patients[i].medications[0].name}`,
-                });
-            }
-        }
-    }
-    return output;
-};
+let getOneMedBySpecies;
 
